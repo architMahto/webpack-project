5d826b185124d3ccf51322ceb04ed4a23727724e
architMahto <architmahto@cmail.carleton.ca>
2017-05-04 18:31:45 -0400

    Loaded CSS files using JS.

--- src/image_viewer.js
+++ src/image_viewer.js
@@ -1,5 +1,6 @@
-const image = document.createElement('img');
+import '../styles/image_viewer.css';
 
+const image = document.createElement('img');
 image.src = 'http://lorempixel.com/400/400';
 
 document.body.appendChild(image);
